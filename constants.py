2ac7a9763407cd3b2883b3379020af5d1cc9369d
Luke <lukep2@illinois.edu>
2021-08-31 08:27:21 -0500

    now supports scipy

--- constants.py
+++ constants.py
@@ -1,8 +1,9 @@
 import random
 from sympy import *
+from scipy import stats
 from re import sub
 
-COMMAND_LIST = ['!help', '!ansformat', '!probq', '!miscprobq', '!distq', '!bivarq', '!peq']
+COMMAND_LIST = ['!help', '!ansformat', '!probq', '!miscprobq', '!distq', '!bivarq', '!peq', '!ciq']
 
 prob_questions = open("hardcoded/prob_questions.txt", 'r')
 PROB_QUESTIONS_LINES = prob_questions.read().splitlines()
@@ -25,37 +26,77 @@ PE_QUESTIONS_LINES = pe_questions.read().splitlines()
 pe_questions.close()
 
 def extended_format(input, vars):
-    new_dict = {}
-    for key, value in vars.items():
-        to_add = value
-        if value.startswith("randint"):
-            ints = [int(i) for i in (value[8:len(value) - 1].split(","))]
-            to_add = random.randint(ints[0], ints[1])
-        elif value.startswith("randuni"):
-            vals = value[8:len(value) - 1].split(",")
-            to_add = round(random.uniform(float(vals[0]), float(vals[1])), int(vals[2]))
-        new_dict[symbols(key)] = to_add
+    expression = input
+    def vars_to_nums():
+        new_dict = {}
+        for key, value in vars.items():
+            to_add = value
+            if value.startswith("randint"):
+                ints = [int(i) for i in (value[8:len(value) - 1].split(","))]
+                to_add = random.randint(ints[0], ints[1])
+            elif value.startswith("randuni"):
+                vals = value[8:len(value) - 1].split(",")
+                to_add = round(random.uniform(float(vals[0]), float(vals[1])), int(vals[2]))
+            new_dict[symbols(key)] = to_add
+        return new_dict
 
-    start_indices = []
-    end_indices = []
-    for index, char in enumerate(input):
-        if char == '&' and input[index + 1] == '(':
-            start_indices.append(index + 2)
-        elif char == '&' and input[index + 1] == ')':
-            end_indices.append(index)
+    new_dict = vars_to_nums()
+    a = 0.05
+    print(stats.t.ppf(a, 20))
 
-    nums = []
-    for start_index, end_index in zip(start_indices, end_indices):
-        evaluated = sympify(input[start_index:end_index]).subs(new_dict)
-        nums.append(round_if_needed(evaluated))
+    def substitute_regex(expression, reg, nums):
+        subbed = expression
+        while len(nums) > 0:
+            subbed = sub(reg, str(nums[0]), subbed, 1)
+            del nums[0]
+        return subbed
 
-    reg = r'&\((.*?)&\)'
-    subbed = input
-    while len(nums) > 0:
-        subbed = sub(reg, str(nums[0]), subbed, 1)
-        del nums[0]
+    def sympify_stats(expression):
+        if '@(' not in expression:
+            return expression
+        for index, char in enumerate(expression):
+            if expression[index:index+7] == '@(tinv(':
+                end_index = expression[index+1:].index('@)') + index
 
-    return subbed.split('=>')
+                first_expression = expression[index+7:end_index].split(",")[0]
+                second_expression = expression[index+7:end_index].split(",")[1]
+
+                first_value = float(round_if_needed(sympify(first_expression).subs(new_dict)))
+                second_value = float(round_if_needed(sympify(second_expression).subs(new_dict)))
+
+                t_stat = round(stats.t.ppf(first_value, second_value), 8)
+                nums = [t_stat]
+                expression = substitute_regex(expression, r'@\((.*?)@\)', nums)
+                break
+
+        return sympify_stats(expression)
+
+    expression = sympify_stats(expression)
+
+    def generate_special_indices():
+        start_indices = []
+        end_indices = []
+        for index, char in enumerate(expression):
+            if char == '&' and expression[index + 1] == '(':
+                start_indices.append(index + 2)
+            elif char == '&' and expression[index + 1] == ')':
+                end_indices.append(index)
+        return start_indices, end_indices
+
+    start_indices, end_indices = generate_special_indices()
+
+    def convert_sympy_to_nums(start_indices, end_indices):
+        nums = []
+        for start_index, end_index in zip(start_indices, end_indices):
+            evaluated = sympify(expression[start_index:end_index]).subs(new_dict)
+            nums.append(round_if_needed(evaluated))
+        return nums
+
+    nums = convert_sympy_to_nums(start_indices, end_indices)
+
+    expression = substitute_regex(expression, r'&\((.*?)&\)', nums)
+
+    return expression.split('=>')
 
 def round_if_needed(val):
     def is_int(val):
--- randomized_questions/confidence_int_questions.py
+++ randomized_questions/confidence_int_questions.py
@@ -1,5 +1,3 @@
-# File for testing randomized questions
-
 import discord
 import random
 from sympy import *
@@ -7,23 +5,29 @@ from discord.ext import commands
 from constants import COMMAND_LIST, extended_format
 
 random_problems = {
-        'Consider a t-distribution with &(a&) degrees of freedom. Find $t/2$ such that the area ' +
-        'to the left of $t$ is &(0.5*b&).=>&(@(tinv(0.5*b,a)@)/2&)'
-        : {'a': 'randint(10,40)', 'b': 'randuni(0.001,0.999,3)'},
+        'Consider $X_1,...,X_5$ with pdf $f(x;\\theta)=\\frac{{1}}{{\\theta}}e^{{-\\frac{{x}}{{\\theta}}}}, ' +
+        'x\geq0, \\theta>0$. Find the MLE estimator of $\\theta$ when $x_1 = &(a&), x_2 = &(b&), ' +
+        'x_3 = &(c&), x_4 = &(d&), x_5 = &(e&)$=>&((a+b+c+d+e)/5&)':
+        {'a': 'randuni(-1,1,2)', 'b': 'randint(3,6)', 'c': 'randint(3,6)', 'd': 'randint(3,6)',
+        'e': 'randint(3,6)'},
+
         }
 
-
-class Test(commands.Cog):
+class Confidence_Intervals(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="test", help="Testing purposes")
-    async def test(self, ctx):
+    @commands.command(name="ciq", help="Answer a confidence interval question")
+    async def ciq(self, ctx):
         random_question, variables = random.choice(list(random_problems.items()))
         formatted_question, formatted_answer = extended_format(random_question, variables)
 
-        preview(formatted_question, viewer="file", filename="generated_latex/output.png")
-        await ctx.send(file=discord.File(f"./generated_latex/output.png", filename="LaTeX_output.png"))
+        try:
+            preview(formatted_question, viewer="file", filename="generated_latex/output.png")
+            await ctx.send(file=discord.File(f"./generated_latex/output.png", filename="LaTeX_output.png"))
+        except:
+            await ctx.send("Please slow down!")
+            return
 
         def check(msg):
             return msg.author == ctx.author and msg.channel == ctx.channel
@@ -45,4 +49,4 @@ class Test(commands.Cog):
                 pass
 
 def setup(bot):
-    bot.add_cog(Test(bot))
+    bot.add_cog(Confidence_Intervals(bot))
